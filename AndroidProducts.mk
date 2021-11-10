84306cfd71b5247e1ada579f2e0ef7adbcf526d3
princeofgiri <ahmadharis1982@gmail.com>
2021-11-10 07:00:30 +0700

    adapt to nenggala

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/nenggala_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    nenggala_vayu-user \
+    nenggala_vayu-userdebug \
+    nenggala_vayu-eng
